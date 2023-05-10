55663471c887d2cef3e156bc457a6c4d22b58b65
damien lewis <damien_lewis2k12@hotmail.com>
2023-05-10 11:56:25 +0100

    adding in get all users function

--- src/users/routes.js
+++ src/users/routes.js
@@ -2,7 +2,7 @@ const { Router } = require("express")
 
 const userRouter = Router()
 
-const {registerUser, login, deleteUser} = require("./controllers") 
+const {registerUser, login, deleteUser, getAllUsers} = require("./controllers") 
 const {hashPass, comparePass, tokenCheck } = require("../middleware")
 
 userRouter.post("/users/register", hashPass, registerUser)
@@ -10,6 +10,8 @@ userRouter.post("/users/register", hashPass, registerUser)
 userRouter.post("/users/login", comparePass, login)
 
 userRouter.get("/users/authcheck", tokenCheck, login)
+
+userRouter.get("/users/getallusers", getAllUsers)
 
 userRouter.delete("/users/deleteuser", deleteUser)
 
